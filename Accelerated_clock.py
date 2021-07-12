467963048480dde5c4e47a00acf37ec7427e5e9b
DesLandysh <60898828+DesLandysh@users.noreply.github.com>
2021-07-12 22:08:30 +0300

    work if inputter writes correctly Ru ver

--- Accelerated_clock.py
+++ Accelerated_clock.py
@@ -1,27 +1,42 @@
 # Author: Des Kitten
 # Program that count new time when you set other than 1.0 speed while watching video/listening audio
 
-# Do not work with .XX decimals...
+
+
+# Checking the missclicks
+
+def replace_missclicks(x: str) -> str:
+    """
+    Funsction returns string with replaced separator
+    :type x: string
+    """
+    tup_of_missclicks = ',', '?', '/', '@', '<', '>', 'б', 'ю', 'Б', \
+                        'Ю', '%', '^', '&', ':', ';', 'ж', 'Ж', '*'
+    for i in tup_of_missclicks:
+        if x.find(str(i)):
+            x = x.replace(str(i), '.')
+
 
 # Start, requesting input data
 time_txt_input = input("Введите время видео в формате ЧЧ:ММ: ")
 
-# ввести проверку на ввод минут без часов, несмотря на заданный формат
-# set len check
-# set print hours&min in text format and axel_mod commented to the end user
-
 axel_mod = input("Введите скорость воспроизведения: ")
 
-# Checking the missclicks
-tup_of_missclicks = ',', '?', '/', '@', '<', '>', 'б', 'ю', 'Б', 'Ю', '%', '^', '&', ':', ';'
+""" Previous code before it def as function above
 for i in tup_of_missclicks:
     if axel_mod.find(str(i)):
         axel_mod = axel_mod.replace(str(i), '.')
     elif time_txt_input.find(str(i)):
         time_txt_input = time_txt_input.replace(str(i), ':')
+"""
+
+replace_missclicks(axel_mod)
+replace_missclicks(time_txt_input)
+print(axel_mod, "time", time_txt_input) ## DEBUG: function
 
 # Converting the acceleration modifier
 acceleration_mod = float(axel_mod)
+print(acceleration_mod)
 
 # Find & count minutes in hours and sums them all
 clock_sep = time_txt_input.find(":")
@@ -32,6 +47,7 @@ num_of_minutes = int(time_txt_input[(clock_sep + 1):])
 sum_of_minutes = num_of_hours * 60 + num_of_minutes
 
 # Separation the new sum of minutes onto hours and minutes
+h_output, m_output = 1, 1
 time_acceleration = int(sum_of_minutes // acceleration_mod)
 if time_acceleration >= 60:
     h_output: int = int(time_acceleration / 60)
