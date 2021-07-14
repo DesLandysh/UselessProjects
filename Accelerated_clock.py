4b103ce129dd38784e0774ffe8baf1553654dc16
DesLandysh <60898828+DesLandysh@users.noreply.github.com>
2021-07-14 18:07:34 +0300

    v.04.5

--- Accelerated_clock.py
+++ Accelerated_clock.py
@@ -3,10 +3,11 @@
 """
     TODO:
     - Try/Exception
-        - by length
-        - by negative values
-    - Refactor sum_of_minutes
-    - Refactor time_acceleration
+        - done! by length
+        - done! by negative values
+        - by letters
+    - undone! Refactor sum_of_minutes
+    - undone! Refactor time_acceleration
     - Return the Ru in other branch
 """
 
@@ -16,7 +17,7 @@ def replace_missclicks(*args) -> str:
     Function returns string with replaced separator
     """
     tup_of_missclicks = ',', '?', '/', '@', '<', '>', 'б', 'ю', 'Б', 'Ю', '%',\
-                        '^', '&', ':', ';', 'Ж', 'ж', '*', '+', "\\", '"', "'"
+                        '^', '&', ':', ';', 'Ж', 'ж', '*', '+', "\\", '"', "'", "."
     x, y = args
     for i in tup_of_missclicks:
         if x.find(str(i)):
@@ -25,37 +26,67 @@ def replace_missclicks(*args) -> str:
     return x
 
 
-def clock_separation():
-    """
-    Function finds and separates minutes from hours by separation marker
-    :get: string
-    :return: tuple of two separated integer values
-    """
-    sep_marker = time_txt_input.find(":")
-    num_of_hours = int(time_txt_input[:sep_marker])
-    num_of_minutes = int(time_txt_input[(sep_marker + 1):])
-    # print(sep_marker, "is the sep;", num_of_hours, "are the hours; and the minutes are ", num_of_minutes) # Debug_line
-    return num_of_hours, num_of_minutes
-
-
 # Start, requesting input data
 time_txt_input = input("Input the time value in format HH:MM: ")
 axel_mod = input("Input the speed value in format X.Y or X.YY: ")
 
+try:
+    len(time_txt_input) > 10
+except TypeError:
+    time_txt_input = "00:01"
+finally:
+    time_txt_input = input("Please, type using HH:MM format:")
+
+try:
+    time_txt_input.find([0] == "-")
+except ValueError:
+    print("You enter the negative value of time")
+finally:
+    print('If you insist of your value, type "Y", otherwise press "N" to set new value')
+    is_choice = input()
+    if is_choice == "N" or "n":
+        time_txt_input = input("Please, type using HH:MM format:")
+    elif is_choice == "Y" or "y":
+        time_txt_input = time_txt_input[:[1]]
+    else:
+        print("Don't trick me out!")
+        time_txt_input = input("Type with >> HH:MM << this format:")
+
+try:
+    len(axel_mod) > 4
+except TypeError:
+    axel_mod = 1.0
+finally:
+    axel_mod = input("Please, type using X.Y or X.YY format:")
+
+try:
+    axel_mod.find([0] == "-")
+except ValueError:
+    print("You enter the negative value of time")
+finally:
+    print('If you insist of your value, type "Y", otherwise press "N" to set new value')
+    is_choice_a = input()
+    if is_choice_a == "N" or "n":
+        axel_mod = input("Please, type using X.YY or Y.X format:")
+    elif is_choice_a == "Y" or "y":
+        axel_mod = axel_mod[:[1]]
+    else:
+        print("Don't trick me out!")
+        axel_mod = input("Type with >> Y.XX << this format:")
+
 # Checking the missclicks
 axel_mod = replace_missclicks(axel_mod, '.')
 time_txt_input = replace_missclicks(time_txt_input, ':')
-# print("axel_mod", axel_mod, "and time", time_txt_input) # Debug_line
 
 # Converting the acceleration modifier
 acceleration_mod = float(axel_mod)
-# print(acceleration_mod, "this is the float num") # Debug_line
 
 # Find & count minutes in hours and sums them all
-num_of_hours = num_of_minutes = 0
-
-clock_separation()
+sep_marker = time_txt_input.find(":")
+num_of_hours = int(time_txt_input[:sep_marker])
+num_of_minutes = int(time_txt_input[(sep_marker + 1):])
 sum_of_minutes = num_of_hours * 60 + num_of_minutes
+
 
 # Separation the new sum of minutes onto hours and minutes
 h_output, m_output = 1, 1
@@ -64,7 +95,15 @@ if time_acceleration >= 60:
     h_output: int = int(time_acceleration / 60)
     m_output: int = int(time_acceleration % 60)
 
-# Разложение на падежи
-print("On the speed of", acceleration_mod, "from base, you'll get only:", h_output, "hour/-s", " and", m_output,
+# Calculating the saving time and separating it onto hours and minutes
+h_saved, m_saved = 1, 1
+saved_time = sum_of_minutes - time_acceleration
+if saved_time >= 60:
+    h_saved: int = int(saved_time / 60)
+    m_saved: int = int(saved_time % 60)
+
+# Output
+print("On the speed of", acceleration_mod, "from base value, you'll spend only:", h_output, "hour/-s", " and", m_output,
       "minute/-s")
+print("You save", h_saved, "hour/-s", m_saved, "minute/s for other things")
 quit()
