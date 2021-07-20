92ecfa4b7ead1deb4de83800dac24d293d5a34cc
DesLandysh <60898828+DesLandysh@users.noreply.github.com>
2021-07-20 16:54:39 +0300

    v0.4.9b Fix: letters input issue

--- Save_your_time.py
+++ Save_your_time.py
@@ -18,28 +18,27 @@ print("""
         """)
 
 
-def match(text_to_check, alphabet=None):
+def match(x, alphabet=None):
     """
-    Function checks if the inputting value have any letters
-    :param text_to_check: is the time_txt_input
+    Function checks if the inputting value have any letters (Stealed from StackOverFlow)
+    :param x: input data
     :param alphabet: eng/ru
-    :return:
+    :return: True if input data have any letter
     """
     if alphabet is None:
         alphabet = set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
                        'abcdefghijklmnopqrstuvwyxz'
                        'АБВГДЕЁЖЗИЙКЛАМНОПРСТУФХЦЧЩЪЫЬЭЮЯ'
                        'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    return not alphabet.isdisjoint(text_to_check)
+    return not alphabet.isdisjoint(x)
 
 
-def replace_missclicks(*args) -> str:
+def replace_missclicks(x, y) -> str:
     """
     Function returns string with replaced separator
     """
     tup_of_missclicks = ',', '?', '/', '@', '<', '>', 'б', 'ю', 'Б', 'Ю', '%', \
                         '^', '&', ':', ';', 'Ж', 'ж', '*', '+', "\\", '"', "'", "."
-    x, y = args
     for i in tup_of_missclicks:
         if x.find(str(i)):
             x = x.replace(str(i), y)
@@ -48,45 +47,53 @@ def replace_missclicks(*args) -> str:
 
 
 # This doesn't work yet fully
-def is_valid_input(*args):
-    x = str(args)
-    is_neg_input(x)
-    # print(x, "complex foo")
-    return x
+def is_valid_input(x):
+    # print(x, "x")  # Debug_line
+    x = is_neg_input(x)
+    # print(x, "complex foo")  # Debug_line
+    x = is_letter_input(x) # до ппринта не доходит
+    # print(x, "comples lettes")  # Debug_line
+    return str(x)
 
-def is_neg_input(*args) -> str:
+def is_neg_input(x) -> str:
     """
     Function checks if the length is much than needed, if there's negative numbers, and if there's letters.
     :return: x: string
     """
-    x = str(args)
     while "-" in x:
         x = input("Please, enter the positive numbers: ")
     else:
-        # print(x, "in foo is valid")
-        return x
+        # print(x, "in foo is valid")  # Debug_line
+        return str(x)
 
 
+def is_letter_input(x):
+    while match(x):
+        x = input("Please, use numbers instead of letters: ")
+    else:
+        # print(x, " in is letter")  # Debug_line
+        return str(x)
 
 # Start, requesting input data
 time_txt_input = input("Input the time value in format HH:MM: ")
 print("")
 time_txt_input = is_valid_input(time_txt_input)
 # Is_valid_input returns here additional symbols before and after string, this solves it
-time_txt_input = time_txt_input[2:-3]
+# time_txt_input = time_txt_input[5:-8]
+# print(time_txt_input, "after is valid")  # Debug_line
 
 axel_mod = input("Input the speed value in format X.Y or X.YY: ")
 print("")
 
 axel_mod = is_valid_input(axel_mod)
 # Is_valid_input returns here additional symbols before and after string, this solves it
-axel_mod = axel_mod[2:-3]
-# print(axel_mod, "after is valid")
+# axel_mod = axel_mod[5:-8]
+# print(axel_mod, "after is valid")  # Debug_line
 
 # Checking the missclicks
 axel_mod = replace_missclicks(axel_mod, '.')
 time_txt_input = replace_missclicks(time_txt_input, ':')
-# print(time_txt_input, "after replace missclicks")
+# print(time_txt_input, "after replace missclicks")   # Debug_line
 
 # Converting the acceleration modifier
 acceleration_mod = float(axel_mod)
